97a1e4384bd68ba84d2fda76b477b2bdae0eb113
longcule <77668059+longcule@users.noreply.github.com>
2025-09-25 16:01:44 +0700

    Update main.py

--- main.py
+++ main.py
@@ -143,10 +143,15 @@ def upload_images(image_base64):
 
 def get_image_response(url: str, auth=None):
     try:
-
+        headers = {
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36",
+            "Accept-Language": "en-US,en;q=0.9",
+            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+            "Referer": "https://www.google.com/",
+        }
         response = requests.get(
             url,
-            headers={"User-Agent": self.USER_AGENT},
+            headers=headers,
             auth=auth,
             timeout=10,
             verify=False,
@@ -478,4 +483,5 @@ async def login( user: LoginUserRequest):
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+
 
